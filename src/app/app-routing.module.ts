90ee4d40568d1024c9f99bb3392d0eff6367d908
Aleksa <lukicaleksa04@gmail.com>
2020-07-08 15:25:18 +0200

    Podesene komponete za redesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
-import { AboutComponent } from './about/about.component';
-import { OrderSiteComponent } from './order-site/order-site.component';
 import { AdminService } from './service/admin.service';
 
 
@@ -15,14 +13,11 @@ const routes: Routes = [
     //loadChildren:()=>import('./login/login.module').then(m=>m.LoginRoutingModule)
   },
   {
-    path: 'about', component: AboutComponent,
-     loadChildren:()=>import('./about/about.module').then(m=>m.AboutRoutingModule)
+    path: 'about', component: AboutComponent
     
   },
   {
-    path: 'order',component:OrderSiteComponent,
-   loadChildren:()=>import('./order-site/order.module').then(m=>m.OrderRoutingModule) 
-    
+    path: 'order',component:OrderSiteComponent
   },
   { path: 'admin', component: AdminComponent, canActivate: [AdminService] }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,35 +7,26 @@ import { MaterialModule } from './material.module';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { MessagePreviewDialogComponent } from './admin/message-preview-dialog/message-preview-dialog.component';
-import { BasicElementComponent } from './basic-element/basic-element.component';
-import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { LoginComponent } from './login/login.component';
-import { OrderSiteComponent } from './order-site/order-site.component';
 import { OrderPreviewComponent } from './admin/order-preview/order-preview.component';
 import { MailAnswerComponent } from './admin/order-preview/mail-answer/mail-answer.component';
 import { ChangeLoginComponent } from './admin/change-login/change-login.component';
 import { LoginRoutingModule } from "./login/login.module";
-import { AboutRoutingModule } from "./about/about.module";
-import { OrderRoutingModule } from "./order-site/order.module";
 import { CommonModule, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     FooterComponent,
     HomeComponent,
-    BasicElementComponent,
-    AboutComponent,
     CopyrightComponent,
     LoginComponent,
     AdminComponent,
     MessagePreviewDialogComponent,
-    OrderSiteComponent,
     OrderPreviewComponent,
     MailAnswerComponent,
     ChangeLoginComponent
@@ -46,8 +37,6 @@ import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
     ReactiveFormsModule,
     AppRoutingModule,
     LoginRoutingModule,
-    OrderRoutingModule,
-    AboutRoutingModule,
     HttpClientModule,
     CKEditorModule,
     MaterialModule,
