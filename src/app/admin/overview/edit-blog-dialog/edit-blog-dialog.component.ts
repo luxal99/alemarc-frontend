62bf6409f80208db6141f37b824abec8f0d5d7d1
Aleksa <lukicaleksa04@gmail.com>
2020-10-25 17:43:03 +0100

    Prepare object for update

--- src/app/admin/overview/edit-blog-dialog/edit-blog-dialog.component.ts
+++ src/app/admin/overview/edit-blog-dialog/edit-blog-dialog.component.ts
@@ -1,12 +1,15 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MatCheckboxChange, MAT_DIALOG_DATA } from '@angular/material';
 import { CKEditorComponent } from '@ckeditor/ckeditor5-angular';
 import { Blog } from 'src/app/model/Blog';
 import { BlogService } from 'src/app/service/blog.service';
 
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Technology } from 'src/app/model/Technology';
+import { TechnologyDTO } from 'src/app/model/TechnologyDTO';
+import { TechnologyService } from 'src/app/service/technology.service';
 
 @Component({
   selector: 'app-edit-blog-dialog',
@@ -18,7 +21,10 @@ export class EditBlogDialogComponent implements OnInit {
   @ViewChild('editor', { static: false }) editorComponent: CKEditorComponent;
   public Editor = ClassicEditor;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Blog, private blogService: BlogService) { }
+  selectedTechnology: Array<TechnologyDTO> = [];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Blog, private blogService: BlogService,
+    private technologyService: TechnologyService) { }
 
   headerForm = new FormGroup({
     header: new FormControl(this.data.header),
@@ -26,7 +32,23 @@ export class EditBlogDialogComponent implements OnInit {
   })
 
   ngOnInit() {
+    this.getTechnologies();
+  }
 
+  getTechnologies() {
+    this.technologyService.getAll().subscribe((resp: Array<Technology>) => {
+      resp.forEach(tech => {
+
+        let techDTO = new TechnologyDTO(tech)
+        if (this.data.listOfTechnologies.findIndex(x => x.id === tech.id) !== -1) {
+          techDTO.checked = true;
+          this.selectedTechnology.push(techDTO)
+
+        } else {
+          this.selectedTechnology.push(techDTO);
+        }
+      })
+    })
   }
 
   errorHandler(event) {
@@ -37,5 +59,28 @@ export class EditBlogDialogComponent implements OnInit {
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.data.listOfImages, event.previousIndex, event.currentIndex);
   }
+
+  addAccessories($event: MatCheckboxChange, accessories: Technology) {
+
+    var index = this.data.listOfTechnologies.indexOf(accessories)
+
+    if ($event.checked && index === -1) {
+      this.data.listOfTechnologies.push(accessories)
+    } else {
+      this.data.listOfTechnologies.splice(index, 1)
+    }
+  }
+
+
+  deletePhoto(photo) {
+    let index = this.data.listOfImages.indexOf(photo);
+    this.data.listOfImages.splice(index, 1)
+  }
+  update() {
+    console.log(this.data);
+
+  }
+
+
 
 }
--- src/app/model/Technology.ts
+++ src/app/model/Technology.ts
@@ -1,4 +1,5 @@
 export class Technology{
+    id:number;
     title;
 
     constructor(title:string){
