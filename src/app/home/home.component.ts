2ba44901a1a3137ce6936af77ebe4a6db19f7593
Aleksa <lukicaleksa04@gmail.com>
2020-12-07 14:16:17 +0100

    New UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { HomeTranslate } from '../translate/home';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
-import * as $ from 'jquery';
-import Swiper, { SwiperOptions } from 'swiper';
+import { Component, OnInit } from '@angular/core';
+
 import { Blog } from '../model/Blog';
 import { BlogService } from '../service/blog.service';
-import { error } from 'protractor';
+import { HomeTranslate } from '../translate/home';
 import { Router } from '@angular/router';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-home',
@@ -16,11 +15,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  language = '';
-  multiLanguage: Array<any> = [HomeTranslate.languagesWords];
-
   listOfBlogs: Array<Blog> = [];
-
 
   barChartLegend = true;
   barChartPlugins = [];
@@ -40,44 +35,13 @@ export class HomeComponent implements OnInit {
   barChartDataMarko: ChartDataSets[] = [{ data: [30, 70], backgroundColor: ['#EC6B56', "#ffa900", "#47B39C"] }];
 
 
-  config: SwiperOptions = {
-    slidesPerView: 3,
-    spaceBetween: 200,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: false,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  }
-
-  mobileConfig: SwiperOptions = {
-    slidesPerView: 1,
-    spaceBetween: 100,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: false,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  }
   constructor(private blogService: BlogService, private router: Router) { }
 
   ngOnInit() {
     window.scrollTo(0, 0)
-    this.getAll()
+    this.getAll();
   }
-  
+
   scrollToElement($element): void {
     $element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
   }
@@ -87,7 +51,7 @@ export class HomeComponent implements OnInit {
 
   getAll() {
     this.blogService.getAll().subscribe(data => {
-      this.listOfBlogs = JSON.parse(JSON.stringify(data)) as Array<Blog>;
+      this.listOfBlogs = data
     }, error => {
       this.router.navigate(['/err'])
     })
